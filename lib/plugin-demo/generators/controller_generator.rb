d6811bae286eaee0e4d1888650f60b157a599f8d
Ben Langfeld <ben@langfeld.me>
2012-02-01 19:03:28 +0000

    [BUGFIX] Fix a namespacing issue

--- lib/plugin-demo/generators/controller_generator.rb
+++ lib/plugin-demo/generators/controller_generator.rb
@@ -4,7 +4,7 @@ module PluginDemo
     argument :controller_name, :type => :string
 
     def create_controller
-      raise Exception, "Generator commands need to be run in an Adhearsion app directory" unless ScriptAhnLoader.in_ahn_application?('.')
+      raise Exception, "Generator commands need to be run in an Adhearsion app directory" unless Adhearsion::ScriptAhnLoader.in_ahn_application?('.')
       self.destination_root = '.'
       template 'lib/controller.rb', "lib/#{@controller_name.underscore}.rb"
       template 'spec/controller_spec.rb', "spec/#{@controller_name.underscore}_spec.rb"
